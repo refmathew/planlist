a68d2f8792d67d805487fe09c280f34efe286cc2
Mathew <reformamathew@protonmail.com>
2020-09-23 13:40:58 +0800

    added functionality of add button

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 // Selectors
-const searchInput = document.querySelector(".search__input");
-const searchAdd = document.querySelector(".search__add");
-const planContainer = document.querySelector(".plan");
-const planList = document.querySelector(".plan__list");
+const inputText = document.querySelector(".input__text");
+const inputAdd = document.querySelector(".input__add");
+const planList = document.querySelector(".planlist");
 
 // Event Listeners
-searchAdd.addEventListener("click", addPLan);
+inputAdd.addEventListener("click", addPLan);
 
 // Functions
 function addPLan(Event){
@@ -15,11 +14,27 @@ function addPLan(Event){
     // add a list container
     const planContainer = document.createElement('div');
     planContainer.classList.add('plan-container');
+    planList.appendChild(planContainer)
     
     // add an entry list
     const plan = document.createElement('li');
-    plan.classList.add('plan')
+    plan.classList.add('plan');
+    plan.innerText = inputText.value;
+    planContainer.appendChild(plan);
 
-    // add bin button
+    // add trash button
+    const trashBtn = document.createElement('button');
+    trashBtn.classList.add('trash-btn');
+    trashBtn.innerHTML= "<i class='fas fa-trash'></i>";
+    planContainer.appendChild(trashBtn);
+
+    // add done button
+    const doneBtn = document.createElement('button');
+    doneBtn.classList.add('done-btn');
+    doneBtn.innerHTML= "<i class='fas fa-check'></i>";
+    planContainer.appendChild(doneBtn);
+
+    // remove input text after entering value
+    inputText.value = ""
 }
 
